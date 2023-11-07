9fd0ffd14d05656fba3324f6ca4229c604cfb918
SAShulyak <8shulyak8@gmail.com>
2023-11-08 00:06:02 +0300

    Task U4. Screenshots - Added AShot's dependency on pom.xml - Added Listener to attach screenshots, when test in failed - Added Test AllureScreenshotDemoTest for demonstrate a feature of attaching screenshots to the Allure report - Some small fixes

---
+++ src/test/java/tests/AllureScreenshotDemoTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.Story;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.qameta.allure.SeverityLevel.BLOCKER;
+import static io.qameta.allure.SeverityLevel.CRITICAL;
+import static steps.Checkers.checkElementIsDisplayed;
+
+@Epic(value="Демонстрации скриншотов в отчете Allure")
+@Feature(value="Наличие элементов")
+public class AllureScreenshotDemoTest extends BaseTest{
+    @Story(value="Элементы блока с курсами, демонстрация фичи со скриншотами в Allure")
+    @Severity(CRITICAL)
+    @Test(description = "Проверка слайдера, падающий тест")
+    public void checkCourseBlockSlider() {
+        mainPage.scrollToCourseBlock().clickButtonPreviousSlide();
+        Assert.assertTrue(checkElementIsDisplayed(mainPage.getAppiumCourse()));
+    }
+
+    @Story(value="Переходы по меню 'Resources', демонстрация фичи со скриншотами в Allure")
+    @Severity(BLOCKER)
+    @Test(description = "Проверка перехода по меню 'Resources' на страницу 'Practice Site 1', падающий тест")
+    public void goToPage() {
+        mainPage.onNavBar().scrollToNavbar().clickResources().clickPracticeSiteOne();
+        Assert.assertEquals(practiceSiteOnePage.getTitleLoadForm(), "Title");
+    }
+}
